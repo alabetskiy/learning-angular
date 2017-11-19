fe2d981de505a785046bfc69403d84fc6a364d92
Artem Labetskiy <labetskiy@gmail.com>
2017-11-18 19:34:46 -0500

    Working with multiple Observables

--- src/app/my-github-followers/my-github-followers.component.ts
+++ src/app/my-github-followers/my-github-followers.component.ts
@@ -1,32 +1,43 @@
+import { Observable } from 'rxjs/Observable';
 import { GitHubService } from './../services/github.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'my-github-followers',
   templateUrl: './my-github-followers.component.html',
   styleUrls: ['./my-github-followers.component.css']
 })
-export class MyGithubFollowersComponent implements OnInit  {
-  
+export class MyGithubFollowersComponent implements OnInit {
+
   constructor(private route: ActivatedRoute, private service: GitHubService) { }
 
-  followers : any[];
+  followers: any[];
 
   ngOnInit() {
-    
-    //retrieving values from query params
-   this.route.queryParams.subscribe((params:Params)=> {
-    let pageNumber = params['page'];
-    console.log(pageNumber);
-   })
-   
+    //I'm combining two observables in one
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe(combined => {
+      let id = combined[0].get('id');
+      combined[1].get('page');
 
-    this.service.getAll()
-    .subscribe(response => {
-      this.followers=response.json();
-      console.log(this.followers);
+      this.service.getAll()
+        .subscribe(response => {
+          this.followers = response.json();
+          console.log(this.followers);
+        })
     })
+
+    //retrieving values from query params
+    //  this.route.queryParams.subscribe((params:Params)=> {
+    //   let pageNumber = params['page'];
+
+    //  })
+
+
   }
 
 }
